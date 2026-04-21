d4dfec5c676e6c564f9eb4b58168755ce2c95576
cbertrand451 <colinboebelbertrand@gmail.com>
2026-04-20 19:06:03 -0500

    set up console

---
+++ python-libraries/cli_data_entry.py
@@ -0,0 +1,9 @@
+from rich.console import Console
+from rich.table import Table
+import csv
+import os
+
+console = Console()
+
+# show example data first 
+console.print("Here is some example data:", style="bold #13294B")
